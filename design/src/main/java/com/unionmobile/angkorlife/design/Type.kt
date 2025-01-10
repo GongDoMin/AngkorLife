a14bb3ba0c40eb1ed7e3ca4096af3d1fe73aac02
GongDoMin <yhw198@naver.com>
2025-01-10 23:26:52 +0900

    feat: KantumruyFontFamily, RobotoFontFamily 추가

---
+++ design/src/main/java/com/unionmobile/angkorlife/design/Type.kt
@@ -0,0 +1,17 @@
+package com.unionmobile.angkorlife.design
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+val KantumruyFontFamily = FontFamily(
+    Font(R.font.kantumruy_pro_regular, FontWeight.Normal),
+    Font(R.font.kantumruy_pro_medium, FontWeight.Medium),
+    Font(R.font.kantumruy_pro_semibold, FontWeight.SemiBold),
+    Font(R.font.kantumruy_pro_bold, FontWeight.Bold)
+)
+
+val RobotoFontFamily = FontFamily(
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+)
