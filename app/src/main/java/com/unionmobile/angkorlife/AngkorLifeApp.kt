33f972d89ed9c91434faef42df476e68a6af3997
GongDoMin <yhw198@naver.com>
2025-01-12 00:25:50 +0900

    refact: windowSoftInputMode, enableEdgeToEdge 추가 및 이로 인해 Scaffold 추가

--- app/src/main/java/com/unionmobile/angkorlife/AngkorLifeApp.kt
+++ app/src/main/java/com/unionmobile/angkorlife/AngkorLifeApp.kt
@@ -1,6 +1,9 @@
 package com.unionmobile.angkorlife
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import com.unionmobile.angkorlife.feature.login.loginScreen
 import com.unionmobile.angkorlife.feature.main.mainScreen
@@ -11,13 +14,15 @@ import com.unionmobile.angkorlife.feature.navigation.rememberAngkorLifeNavContro
 fun AngkorLifeApp() {
     val navController = rememberAngkorLifeNavController()
 
-    NavHost(
-        navController = navController.navController,
-        startDestination = Routes.LOGIN,
-    ) {
-        loginScreen()
+    Scaffold { paddingValues ->
+        NavHost(
+            modifier = Modifier.padding(paddingValues),
+            navController = navController.navController,
+            startDestination = Routes.LOGIN,
+        ) {
+            loginScreen()
 
-        mainScreen()
+            mainScreen()
+        }
     }
-
 }
--- app/src/main/java/com/unionmobile/angkorlife/MainActivity.kt
+++ app/src/main/java/com/unionmobile/angkorlife/MainActivity.kt
@@ -3,6 +3,7 @@ package com.unionmobile.angkorlife
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,6 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             AngkorLifeApp()
         }
