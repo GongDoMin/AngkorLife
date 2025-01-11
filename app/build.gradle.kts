990b68a3395ce38dc58c15f2c0eec2ba23bc4b84
GongDoMin <yhw198@naver.com>
2025-01-11 17:20:17 +0900

    feat: app module navigation dependencies 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    
+
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
+
+    implementation(libs.androidx.hilt.navigation.compose)
 }
