a697f21b7ea547bab48389c54b4d042c09f40134
GongDoMin <yhw198@naver.com>
2025-01-12 00:56:23 +0900

    refact: mainScreen 로직 수정

--- feature/src/main/java/com/unionmobile/angkorlife/feature/main/MainNavigation.kt
+++ feature/src/main/java/com/unionmobile/angkorlife/feature/main/MainNavigation.kt
@@ -6,6 +6,8 @@ import com.unionmobile.angkorlife.feature.navigation.Routes
 
 fun NavGraphBuilder.mainScreen() {
     composable<Routes.MAIN> {
-        mainScreen()
+        MainScreen(
+            candidates = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8)
+        )
     }
 }
