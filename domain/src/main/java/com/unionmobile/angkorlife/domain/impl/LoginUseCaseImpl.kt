bb3bdb831ef689ffbaf81a867d15aaf8ffbba91a
GongDoMin <yhw198@naver.com>
2025-01-12 14:14:08 +0900

    feat: LoginUseCaseImpl UserIdRepository 의존성 주입

--- domain/src/main/java/com/unionmobile/angkorlife/domain/impl/LoginUseCaseImpl.kt
+++ domain/src/main/java/com/unionmobile/angkorlife/domain/impl/LoginUseCaseImpl.kt
@@ -1,16 +1,22 @@
 package com.unionmobile.angkorlife.domain.impl
 
+import com.unionmobile.angkorlife.domain.repository.UserIdRepository
 import com.unionmobile.angkorlife.domain.usecase.LoginUseCase
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class LoginUseCaseImpl @Inject constructor() : LoginUseCase {
+class LoginUseCaseImpl @Inject constructor(
+    private val userIdRepository: UserIdRepository
+) : LoginUseCase {
     override operator fun invoke(id: String): Flow<Unit> =
         flow {
             if (!id.isValidRange()) {
                 throw IllegalArgumentException("id must be between 1 and 16")
             }
+
+            userIdRepository.setUserId(id)
+
             emit(Unit)
         }
 
