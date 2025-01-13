0070c910f31437e81f0ff62ee454cd275e21b508
GongDoMin <yhw198@naver.com>
2025-01-13 14:30:22 +0900

    refact: MainHeader 리랙토링

--- feature/src/main/java/com/unionmobile/angkorlife/feature/main/MainHeader.kt
+++ feature/src/main/java/com/unionmobile/angkorlife/feature/main/MainHeader.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,13 +17,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.unionmobile.angkorlife.design.KantumruyFontFamily
 import com.unionmobile.angkorlife.design.R
 import com.unionmobile.angkorlife.domain.model.Timer
 import com.unionmobile.angkorlife.feature.common.PainterImage
+import com.unionmobile.angkorlife.feature.common.dpTextUnit
 import java.util.Locale
 
 @Composable
@@ -67,37 +68,37 @@ fun Timer(
         horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
     ) {
         TimeContainerWithLabel(
-            label = "DAY",
-            time = day
+            time = day,
+            label = "DAY"
         )
 
         Colon()
 
         TimeContainerWithLabel(
-            label = "HR",
-            time = hour
+            time = hour,
+            label = "HR"
         )
 
         Colon()
 
         TimeContainerWithLabel(
-            label = "MIN",
-            time = minute
+            time = minute,
+            label = "MIN"
         )
 
         Colon()
 
         TimeContainerWithLabel(
-            label = "SEC",
-            time = second
+            time = second,
+            label = "SEC"
         )
     }
 }
 
 @Composable
 fun TimeContainerWithLabel(
-    label: String,
     time: Int,
+    label: String,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -106,17 +107,19 @@ fun TimeContainerWithLabel(
         verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         TimeContainer(
+            modifier = Modifier
+                .size(48.dp)
+                .background(Color(0xFF1B191A)),
             time = time
         )
 
-        Text(
-            text = label,
-            style = TextStyle(
-                fontFamily = KantumruyFontFamily,
-                fontWeight = FontWeight.Medium,
-                fontSize = 10.sp
-            ),
-            color = Color(0xFFB9B9B9)
+        LabelContainer(
+            modifier = Modifier
+                .size(
+                    width = 48.dp,
+                    height = 12.dp
+                ),
+            label = label
         )
     }
 }
@@ -125,11 +128,10 @@ fun TimeContainerWithLabel(
 @Composable
 fun TimeContainer(
     time: Int,
+    modifier: Modifier = Modifier
 ) {
     Box(
-        modifier = Modifier
-            .size(48.dp)
-            .background(Color(0xFF1B191A)),
+        modifier = modifier,
         contentAlignment = Alignment.Center
     ) {
         Text(
@@ -137,7 +139,7 @@ fun TimeContainer(
             style = TextStyle(
                 fontFamily = KantumruyFontFamily,
                 fontWeight = FontWeight.Medium,
-                fontSize = 22.sp
+                fontSize = 22.dpTextUnit
             ),
             color = Color(0xFFDADADA)
         )
@@ -145,18 +147,61 @@ fun TimeContainer(
 }
 
 @Composable
-fun Colon() {
-    Text(
-        text = ":",
-        color = Color(0xFFD9D9D9)
-    )
+fun LabelContainer(
+    label: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = label,
+            style = TextStyle(
+                fontFamily = KantumruyFontFamily,
+                fontWeight = FontWeight.Medium,
+                fontSize = 10.dpTextUnit
+            ),
+            color = Color(0xFFB9B9B9)
+        )
+    }
+}
+
+@Composable
+fun Colon(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        PainterImage(
+            modifier = Modifier
+                .size(width = 2.dp, height = 10.dp),
+            res = R.drawable.img_colon,
+        )
+
+        Spacer(modifier = Modifier.padding(vertical = 8.dp))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun TimeContainerPreview() {
     TimeContainer(
+        modifier = Modifier
+            .size(48.dp)
+            .background(Color(0xFF1B191A)),
         time = 10
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LabelContainerPreview() {
+    LabelContainer(
+        modifier = Modifier
+            .size(width = 48.dp, height = 12.dp),
+        label = "DAY"
     )
 }
 
@@ -167,8 +212,8 @@ fun TimeContainerPreview() {
 @Composable
 fun TimeContainerWithLabelPreview() {
     TimeContainerWithLabel(
-        label = "SEC",
-        time = 10
+        time = 10,
+        label = "SEC"
     )
 }
 
