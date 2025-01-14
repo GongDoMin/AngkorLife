9178920c1dc8d02ba94ceb070c4479c76b502943
GongDoMin <yhw198@naver.com>
2025-01-14 18:03:53 +0900

    feat: InfiniteHorizontalPager 주석 추가

--- feature/src/main/java/com/unionmobile/angkorlife/feature/detail/InfiniteHorizontalPager.kt
+++ feature/src/main/java/com/unionmobile/angkorlife/feature/detail/InfiniteHorizontalPager.kt
@@ -19,14 +19,20 @@ fun InfiniteHorizontalPager(
     bottomContent: @Composable (currentPage: Int) -> Unit,
     pagerContent: @Composable PagerScope.(page: Int) -> Unit
 ) {
+    // listSize 가 0일 경우에는 pageCount 를 0으로 한다.
     val pageCount = if (listSize > 0) Int.MAX_VALUE else 0
+
+    // initialPage는 전체 페이지 중간 지점을 기준으로 listSize의 배수인 페이지를 설정합니다.
+    // 이렇게 함으로써, 무한 스크롤의 경우 첫 페이지가 정확히 리스트의 시작 상태와 매칭됩니다.
     val initialPage = if (listSize > 0) pageCount / 2 - ((pageCount / 2) % listSize) else 0
+
     val pagerState = rememberPagerState(
         initialPage = initialPage,
         pageCount = { pageCount }
     )
     val isDragged by pagerState.interactionSource.collectIsDraggedAsState()
 
+    // 드래그 중이라면 취소
     if (!isDragged) {
         LaunchedEffect(Unit) {
             while (true) {
