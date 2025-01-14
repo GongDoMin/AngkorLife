1bace4e047f1a379a0dfdee14f0104f89e13414b
GongDoMin <yhw198@naver.com>
2025-01-14 17:58:53 +0900

    refact: DotContainer 리팩토링

--- feature/src/main/java/com/unionmobile/angkorlife/feature/detail/DotContainer.kt
+++ feature/src/main/java/com/unionmobile/angkorlife/feature/detail/DotContainer.kt
@@ -5,16 +5,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -22,17 +21,12 @@ fun DotContainer(
     dotCount: Int,
     currentPage: Int
 ) {
-    val dotSize = remember { 8.dp }
-    val dotBottomPadding = remember { 10.dp }
-
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .height(dotSize + dotBottomPadding)
             .padding(
                 start = 16.dp,
-                end = 16.dp,
-                bottom = dotBottomPadding
+                end = 16.dp
             ),
         horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
     ) {
@@ -52,4 +46,28 @@ fun DotContainer(
             )
         }
     }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF000000
+)
+@Composable
+fun DotContainerCurrentPageIs0Preview() {
+    DotContainer(
+        dotCount = 4,
+        currentPage = 0
+    )
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF000000
+)
+@Composable
+fun DotContainerCurrentPageIs1Preview() {
+    DotContainer(
+        dotCount = 4,
+        currentPage = 1
+    )
 }
