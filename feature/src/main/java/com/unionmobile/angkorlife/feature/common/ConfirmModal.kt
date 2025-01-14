4073e130753a8ac6a941c0c922c46d354d196af5
GongDoMin <yhw198@naver.com>
2025-01-14 16:57:36 +0900

    refact: ConfirmModal 리팩토링

--- feature/src/main/java/com/unionmobile/angkorlife/feature/common/ConfirmModal.kt
+++ feature/src/main/java/com/unionmobile/angkorlife/feature/common/ConfirmModal.kt
@@ -3,11 +3,9 @@ package com.unionmobile.angkorlife.feature.common
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.unionmobile.angkorlife.design.BasicLineHeightStyle
 import com.unionmobile.angkorlife.design.KantumruyFontFamily
 
 @Composable
@@ -47,49 +46,38 @@ fun ConfirmModal(
                 .width(280.dp)
                 .clip(RoundedCornerShape(8.dp))
                 .background(Color.White)
-                .clickable {}
+                .clickable {},
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(modifier = Modifier.padding(vertical = 16.dp))
 
-            Box(
+            Text(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(24.dp)
-                    .padding(horizontal = 16.dp)
-            ) {
-                Text(
-                    modifier = Modifier
-                        .align(Alignment.Center),
-                    text = title,
-                    style = TextStyle(
-                        fontFamily = KantumruyFontFamily,
-                        fontWeight = FontWeight.Medium,
-                        fontSize = 16.dpTextUnit,
-                        lineHeight = 24.dpTextUnit
-                    ),
-                    color = Color.Black,
-                )
-            }
-
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(24.dp)
-                    .padding(horizontal = 16.dp)
-            ) {
-                Text(
-                    modifier = Modifier
-                        .align(Alignment.Center),
-                    text = message,
-                    style = TextStyle(
-                        fontFamily = KantumruyFontFamily,
-                        fontWeight = FontWeight.Light,
-                        fontSize = 14.dpTextUnit,
-                        lineHeight = 20.dpTextUnit
-                    ),
+                    .padding(horizontal = 16.dp),
+                text = title,
+                style = TextStyle(
+                    fontFamily = KantumruyFontFamily,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 16.dpTextUnit,
+                    lineHeight = 24.dpTextUnit,
+                    lineHeightStyle = BasicLineHeightStyle,
                     color = Color.Black
                 )
-            }
+            )
+
+            Text(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp),
+                text = message,
+                style = TextStyle(
+                    fontFamily = KantumruyFontFamily,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 14.dpTextUnit,
+                    lineHeight = 20.dpTextUnit,
+                    lineHeightStyle = BasicLineHeightStyle,
+                    color = Color.Black
+                )
+            )
 
             Spacer(modifier = Modifier.padding(vertical = 9.dp))
 
@@ -99,30 +87,24 @@ fun ConfirmModal(
                     .background(Color(0xFFE5E5EA))
             )
 
-            Box(
+            Text(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(44.dp)
                     .clip(RoundedCornerShape(bottomStart = 8.dp, bottomEnd = 8.dp))
                     .clickable(
                         interactionSource = remember { MutableInteractionSource() },
                         indication = null,
                         onClick = onClickConfirmButton
                     ),
-            ) {
-                Text(
-                    modifier = Modifier
-                        .align(Alignment.Center),
-                    text = confirmButtonText,
-                    style = TextStyle(
-                        fontFamily = KantumruyFontFamily,
-                        fontWeight = FontWeight.Medium,
-                        fontSize = 16.dpTextUnit,
-                        lineHeight = 24.dpTextUnit
-                    ),
+                text = confirmButtonText,
+                style = TextStyle(
+                    fontFamily = KantumruyFontFamily,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 16.dpTextUnit,
+                    lineHeight = 44.dpTextUnit,
+                    lineHeightStyle = BasicLineHeightStyle,
                     color = Color(0xFF4232D5)
                 )
-            }
+            )
         }
     }
 }
